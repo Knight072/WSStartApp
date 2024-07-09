913ba38a8d1022ad0d7b3ceab019881c958ff264
unknown <daniel.rojas-h@labinfo.is.escuelaing.edu.co>
2024-07-09 10:20:28 -0500

    websocket timer

---
+++ src/main/java/co/edu/escuelaing/websocketsprimer/WSStartApp.java
@@ -0,0 +1,9 @@
+package co.edu.escuelaing.websocketsprimer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WSStartApp {
+    public static void main(String[] args){
+        SpringApplication.run(WSStartApp.class, args);
+    }
+}
